a3a778a3bcb99e7fc3431b1420385ca57d3eca5c
MariaBetancur <mabo6241@gmail.com>
2023-09-27 21:26:58 -0500

    v2

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,17 @@
+import { ContactButton, ContactItem } from './Contact.styled';
+import { useDeleteContactMutation } from 'redux/contactApi';
+
+function Contact({ name, number, id }) {
+  const [deleteContact] = useDeleteContactMutation();
+
+  return (
+    <ContactItem key={id}>
+      {name}: {number}
+      <ContactButton type="button" onClick={() => deleteContact(id)}>
+        Delete
+      </ContactButton>
+    </ContactItem>
+  );
+}
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import { GlobalStyle } from './index.styled.jsx';
+import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
